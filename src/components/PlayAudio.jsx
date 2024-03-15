975a254f06bab6650b461365f2a623c7db15ca44
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-15 14:06:04 +0600

    Category and subcategory working fine!

---
+++ src/components/PlayAudio.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef } from 'react';
+const PlayAudio = ({ content }) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [previous, setPrevious] = useState({})
+    const audioRef = useRef(null);
+    const previousRef = useRef(null);
+
+
+    const togglePlay = () => {
+        previousRef.current = audioRef.current;
+        if (!isPlaying) {
+            previousRef.current.pause();
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
+
+    return (
+        <div>
+            {content.audio && (
+                <audio ref={audioRef} src={content.audio}></audio>
+            )}
+            {content.audio && (
+                <img
+                    src="play.svg"
+                    alt=""
+                    onClick={togglePlay}
+                    className="cursor-pointer"
+                />
+            )}
+        </div>
+    );
+};
+
+export default PlayAudio;
