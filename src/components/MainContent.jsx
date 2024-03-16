82308c41165dfc42e2039ddca18c0e9c862cc1d0
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-16 20:42:01 +0600

    Major Updates!

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useState } from "react";
 import PlayAudio from "./PlayAudio";
 const MainContent = ({ currentCat }) => {
@@ -56,15 +58,74 @@ const MainContent = ({ currentCat }) => {
                                         <PlayAudio content={content}></PlayAudio>
                                     }
                                 </div>
-                                <div className="flex gap-6">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <div className="flex gap-4">
+                                    <svg onClick={() => <>
+                                        {navigator.clipboard.writeText(`${content.dua_name_en}\n ${content.top_en}\n${content.clean_arabic ? content.clean_arabic : ""}\n${content.transliteration_en ? content.transliteration_en : ""}\n${content.translation_en ? content.translation_en : ""}\n`)}
+                                        {toast("Text Copied to clipboard")}
+                                    </>} className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                         <path d="M8.00027 16.0011H6.00027C5.46984 16.0011 4.96113 15.7904 4.58606 15.4153C4.21099 15.0402 4.00027 14.5315 4.00027 14.0011V6.0011C4.00027 5.47067 4.21099 4.96196 4.58606 4.58688C4.96113 4.21181 5.46984 4.0011 6.00027 4.0011H14.0003C14.5307 4.0011 15.0394 4.21181 15.4145 4.58688C15.7896 4.96196 16.0003 5.47067 16.0003 6.0011V8.0011M10.0003 20.0011H18.0003C18.5307 20.0011 19.0394 19.7904 19.4145 19.4153C19.7896 19.0402 20.0003 18.5315 20.0003 18.0011V10.0011C20.0003 9.47067 19.7896 8.96196 19.4145 8.58688C19.0394 8.21181 18.5307 8.0011 18.0003 8.0011H10.0003C9.46984 8.0011 8.96113 8.21181 8.58606 8.58688C8.21099 8.96196 8.00027 9.47067 8.00027 10.0011V18.0011C8.00027 18.5315 8.21099 19.0402 8.58606 19.4153C8.96113 19.7904 9.46984 20.0011 10.0003 20.0011Z" stroke="#868686" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                     </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                        <path d="M4.99973 5C4.99973 4.46956 5.21044 3.96086 5.58551 3.58579C5.96058 3.21071 6.46929 3 6.99972 3H16.9997C17.5301 3 18.0388 3.21071 18.4139 3.58579C18.789 3.96086 18.9997 4.46956 18.9997 5V21L11.9997 17.5L4.99973 21V5Z" stroke="#868686" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                        <g clip-path="url(#clip0_1_1008)">
+                                    <ToastContainer
+                                        position="bottom-center"
+                                        autoClose={5000}
+                                        hideProgressBar={false}
+                                        newestOnTop={false}
+                                        closeOnClick
+                                        rtl={false}
+                                        pauseOnFocusLoss
+                                        draggable
+                                        pauseOnHover
+                                        theme="dark"
+                                        transition:Bounce
+                                    />
+                                    <div className="font-inter">
+                                        <label htmlFor="my_modal_6" className="hover:cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path d="M4.99973 5C4.99973 4.46956 5.21044 3.96086 5.58551 3.58579C5.96058 3.21071 6.46929 3 6.99972 3H16.9997C17.5301 3 18.0388 3.21071 18.4139 3.58579C18.789 3.96086 18.9997 4.46956 18.9997 5V21L11.9997 17.5L4.99973 21V5Z" stroke="#868686" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg></label>
+                                        <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+                                        <div className="modal" role="dialog">
+                                            <div className="space-y-4 modal-box">
+                                                <h3 className="font-bold text-lg">Favorite</h3>
+                                                <div className="space-y-4">
+                                                    <label className="form-control w-full">
+                                                        <div className="label">
+                                                            <span className="label-text text-lg font-semibold">Folder Name</span>
+                                                        </div>
+                                                        <label className="input input-bordered flex items-center gap-2">
+                                                            <input type="text" className="grow" value="Favorite" placeholder="Search" />
+                                                            <img width="16" height="16" src="https://img.icons8.com/ios/50/expand-arrow--v1.png" alt="expand-arrow--v1" />
+                                                        </label>
+                                                    </label>
+                                                    <div className="label">
+                                                        <span className="label-text text-lg font-semibold">Or,</span>
+                                                    </div>
+                                                    <label className="input input-bordered flex items-center gap-2">
+                                                        <input type="text" className="grow" placeholder="Create a new bookmark holder" />
+                                                    </label>
+                                                    <div className="label">
+                                                        <span className="label-text text-lg font-semibold">Change Folder Color</span>
+                                                    </div>
+                                                    <div className="w-full flex justify-between">
+                                                        <div className="h-10 w-10 bg-[#1fa45b] rounded-md"></div>
+                                                        <div className="h-10 w-10 bg-[#ffc107] rounded-md"></div>
+                                                        <div className="h-10 w-10 bg-[#9c27b0] rounded-md"></div>
+                                                        <div className="h-10 w-10 bg-[#2196f3] rounded-md"></div>
+                                                        <div className="h-10 w-10 bg-[#e91e63] rounded-md"></div>
+                                                        <div className="h-10 w-10 bg-[#3f51b5] rounded-md"></div>
+                                                        <div className="h-10 w-10 bg-[#00bcd4] rounded-md"></div>
+                                                        <div className="h-10 w-10 bg-[#8bc34a] rounded-md"></div>
+                                                    </div>
+                                                </div>
+                                                <div className="modal-action">
+                                                    <label htmlFor="my_modal_6" className="btn bg-gray-200 text-black px-20">Cancel</label>
+                                                    <label onClick={()=> toast("Bookmark Added Successfully")} htmlFor="my_modal_6" className="btn btn-success text-white px-20">Save</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <svg onClick={()=> toast("Coming Soon Insha-Allah")} className="hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <g clipPath="url(#clip0_1_1008)">
                                             <path d="M17.994 2.28842C16.7734 1.19781 15.2786 0.460703 13.6703 0.156437C12.062 -0.147829 10.4012 -0.00773287 8.86658 0.561647C7.33198 1.13103 5.98172 2.10813 4.96111 3.38779C3.9405 4.66746 3.28819 6.20123 3.0744 7.82402C2.88242 9.24701 3.03607 10.6953 3.52234 12.0463C4.00861 13.3974 4.81319 14.6114 5.868 15.5856C6.52006 16.1576 7.0462 16.8588 7.41306 17.6448C7.77993 18.4308 7.97952 19.2845 7.9992 20.1516V20.3124C8.00015 21.2908 8.38922 22.2288 9.08103 22.9206C9.77283 23.6124 10.7108 24.0015 11.6892 24.0024H12.3096C13.288 24.0015 14.226 23.6124 14.9178 22.9206C15.6096 22.2288 15.9986 21.2908 15.9996 20.3124V19.7628C16.0146 18.9964 16.1951 18.2422 16.5285 17.5519C16.8619 16.8616 17.3406 16.2515 17.9316 15.7632C18.89 14.9237 19.6592 13.8901 20.1881 12.7309C20.7169 11.5717 20.9935 10.3134 20.9994 9.03929C21.0053 7.76516 20.7404 6.50433 20.2223 5.34029C19.7042 4.17626 18.9446 3.13679 17.994 2.28842ZM12.3096 22.002H11.688C11.2401 22.0014 10.8107 21.8232 10.494 21.5065C10.1772 21.1897 9.99903 20.7603 9.9984 20.3124L9.99 20.0028H13.998V20.3124C13.9974 20.7603 13.8191 21.1897 13.5024 21.5065C13.1857 21.8232 12.7563 22.0014 12.3084 22.002H12.3096ZM16.6092 14.262C15.4708 15.2494 14.6485 16.5504 14.2452 18.0024H12.9996V10.818C13.5822 10.6119 14.087 10.2308 14.4448 9.72687C14.8026 9.22297 14.9961 8.62085 14.9988 8.00282C14.9862 7.74619 14.8754 7.50422 14.6893 7.32703C14.5032 7.14985 14.2561 7.05102 13.9992 7.05102C13.7423 7.05102 13.4951 7.14985 13.3091 7.32703C13.123 7.50422 13.0122 7.74619 12.9996 8.00282C12.9996 8.26809 12.8942 8.5225 12.7066 8.71007C12.5191 8.89765 12.2647 9.00303 11.9994 9.00303C11.7341 9.00303 11.4797 8.89765 11.2921 8.71007C11.1046 8.5225 10.9992 8.26809 10.9992 8.00282C10.9992 7.73771 10.8939 7.48346 10.7064 7.296C10.519 7.10854 10.2647 7.00322 9.9996 7.00322C9.73449 7.00322 9.48023 7.10854 9.29277 7.296C9.10531 7.48346 9 7.73771 9 8.00282C9.00267 8.62085 9.19615 9.22297 9.55399 9.72687C9.91183 10.2308 10.4166 10.6119 10.9992 10.818V18.0024H9.678C9.22695 16.5117 8.38108 15.1708 7.23 14.1216C6.20156 13.1631 5.48561 11.9171 5.17536 10.5459C4.86512 9.17465 4.97498 7.74179 5.49063 6.4339C6.00627 5.12601 6.90381 4.00371 8.06634 3.21315C9.22888 2.42259 10.6025 2.00042 12.0084 2.00162C13.7261 1.99659 15.3844 2.63046 16.6608 3.78002C17.4002 4.43978 17.991 5.24911 18.394 6.1544C18.797 7.0597 19.0031 8.0403 18.9986 9.03124C18.994 10.0222 18.779 11.0009 18.3677 11.9024C17.9564 12.804 17.3582 13.6079 16.6128 14.2608H16.6092V14.262Z" fill="#868686" />
                                         </g>
                                         <defs>
@@ -73,10 +134,10 @@ const MainContent = ({ currentCat }) => {
                                             </clipPath>
                                         </defs>
                                     </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <svg onClick={()=> toast("Coming Soon Insha-Allah")} className="hover:cursor-pointer"  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                         <path d="M8.68408 13.3424C8.88608 12.9384 9.00008 12.4824 9.00008 12.0004C9.00008 11.5184 8.88608 11.0624 8.68408 10.6584M8.68408 13.3424V10.6584M8.68408 13.3424C8.38178 13.9467 7.88428 14.4314 7.27217 14.7177C6.66007 15.0041 5.96921 15.0754 5.31152 14.9201C4.65384 14.7648 4.06785 14.3919 3.64849 13.862C3.22914 13.3321 3.00098 12.6761 3.00098 12.0004C3.00098 11.3246 3.22914 10.6686 3.64849 10.1387C4.06785 9.60877 4.65384 9.23596 5.31152 9.08066C5.96921 8.92535 6.66007 8.99665 7.27217 9.283C7.88428 9.56934 8.38178 10.054 8.68408 10.6584M8.68408 13.3424L15.3161 16.6584M8.68408 10.6584L15.3161 7.34236M15.3161 16.6584C14.9602 17.3702 14.9016 18.1943 15.1533 18.9493C15.4049 19.7043 15.9462 20.3284 16.6581 20.6844C17.3699 21.0403 18.194 21.0988 18.949 20.8472C19.704 20.5955 20.3282 20.0542 20.6841 19.3424C21.04 18.6305 21.0986 17.8064 20.8469 17.0514C20.5952 16.2964 20.0539 15.6723 19.3421 15.3164C18.9896 15.1401 18.6059 15.035 18.2128 15.0071C17.8197 14.9792 17.425 15.0289 17.0511 15.1535C16.2961 15.4052 15.672 15.9465 15.3161 16.6584ZM15.3161 7.34236C15.4923 7.69476 15.7362 8.009 16.0339 8.26714C16.3316 8.52527 16.6772 8.72225 17.051 8.84682C17.4248 8.97139 17.8195 9.02111 18.2125 8.99315C18.6055 8.96519 18.9892 8.86009 19.3416 8.68386C19.694 8.50762 20.0082 8.26371 20.2664 7.96603C20.5245 7.66835 20.7215 7.32274 20.846 6.94894C20.9706 6.57514 21.0203 6.18046 20.9924 5.78744C20.9644 5.39442 20.8593 5.01076 20.6831 4.65836C20.3272 3.94665 19.7031 3.40548 18.9482 3.1539C18.1932 2.90232 17.3693 2.96094 16.6576 3.31686C15.9459 3.67278 15.4047 4.29685 15.1531 5.05177C14.9015 5.8067 14.9602 6.63065 15.3161 7.34236Z" stroke="#868686" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                     </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <svg onClick={()=> toast("Coming Soon Insha-Allah")} className="hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                         <path d="M7.86055 1.9989H16.1406L22.0005 7.8589V16.1389L16.1406 21.9989H7.86055L2.00055 16.1389V7.8589L7.86055 1.9989Z" stroke="#868686" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                         <path d="M12 7.9989V11.9989" stroke="#868686" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                         <path d="M12 16.0012H12.01" stroke="#868686" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -84,11 +145,11 @@ const MainContent = ({ currentCat }) => {
                                 </div>
 
                             </div>
-                        </div>
+                        </div >
                     </>
                 ))
             }
-        </div>
+        </div >
     );
 };
 
